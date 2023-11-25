8a727ef4eb4d71f20b6fd40deabe9357d7129bcc
Samuel Ramon <samuelrbo@gmail.com>
2023-11-25 17:12:37 +0000

    feat: :sparkles: 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,23 +1,47 @@
 import Layout from '../components/layout';
 import Head from 'next/head';
 import Link from 'next/link';
-import utilStyles from '../../styles/utils.module.css';
+import utilStyles from '../styles/utils.module.css';
+import { useEffect, useState } from 'react';
 
 export default ({
-  postData
+  postData,
 }: {
   postData: {
-    title: string,
-    date: string,
-    contentHtml: string,
-  },
-}) => (
-  <Layout>
-    <Head>
-      <title>404</title>
-    </Head>
-    <section>
-      <h1>404 - Page Not Found</h1>
-    </section>
-  </Layout>
-);
+    title: string;
+    date: string;
+    contentHtml: string;
+  };
+}) => {
+  const [openStyle, setOpenStyle] = useState<string>('');
+
+  setInterval(() => {
+    openStyle === 'open' ? setOpenStyle('') : setOpenStyle('open');
+  }, 3000);
+
+  return (
+    <Layout>
+      <Head>
+        <title>404</title>
+      </Head>
+      <section className={`${utilStyles.centerContent}`}>
+        <div className={`book ${openStyle}`}>
+          <div className='back'></div>
+          <div className='page6'>
+            <p>Page Not Found</p>
+          </div>
+          <div className='page5'>
+            <p>404</p>
+          </div>
+          <div className='page4'></div>
+          <div className='page3'></div>
+          <div className='page2'></div>
+          <div className='page1'></div>
+          <div className='front'>
+            <h1 className=' title'>Error</h1>
+          </div>
+        </div>
+      </section>
+    </Layout>
+  );
+};
